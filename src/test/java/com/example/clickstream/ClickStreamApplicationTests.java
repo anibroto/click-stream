33a71199ccd412933e96282c8feb2b33968d41b0
Anibroto Sarkar <60008812+anibroto@users.noreply.github.com>
2020-04-29 11:18:02 +0200

    Initial Commit

---
+++ src/test/java/com/example/clickstream/ClickStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clickstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
